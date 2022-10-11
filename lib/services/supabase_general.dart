2af8950b20089652d82802dddc24784930693303
Ruud14 <ruudbrouwers@hotmail.com>
2022-10-11 10:40:47 +0200

    Change to new supabase project

--- lib/services/supabase_general.dart
+++ lib/services/supabase_general.dart
@@ -5,10 +5,15 @@ class SupaBaseService {
   static final supabase = Supabase.instance.client;
 
   static Future<void> setup() async {
+    // await Supabase.initialize(
+    //   url: 'https://wmguavwdykudbvpuqigz.supabase.co',
+    //   anonKey:
+    //       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtZ3VhdndkeWt1ZGJ2cHVxaWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM2ODQ1ODcsImV4cCI6MTk3OTI2MDU4N30.2Nv1-PoHB59o5gpdKEzS81FX9RyioD68Lsm7PlfBmBI',
+    // );
     await Supabase.initialize(
-      url: 'https://wmguavwdykudbvpuqigz.supabase.co',
+      url: 'https://nejidlqgrybsoasntktn.supabase.co',
       anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtZ3VhdndkeWt1ZGJ2cHVxaWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM2ODQ1ODcsImV4cCI6MTk3OTI2MDU4N30.2Nv1-PoHB59o5gpdKEzS81FX9RyioD68Lsm7PlfBmBI',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lamlkbHFncnlic29hc250a3RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU0NzY1MTksImV4cCI6MTk4MTA1MjUxOX0.MQITEEJffdhTrpoXQOVqp3eODnC308VymEg9MedA9QQ',
     );
   }
 }
